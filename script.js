0c5c9cae067459c1170b6d604c8acc0f23152780
shihabshahrier <shahrier13@yahoo.com>
2024-11-21 23:59:17 +0600

    some elements to begin with

---
+++ script.js
@@ -0,0 +1,7 @@
+const header1 = document.getElementById('header1');
+header1.innerHTML += " world";
+
+const head = document.getElementsByClassName('header');
+head[4].
+console.log(header1.innerHTML);
+
